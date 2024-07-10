ba5f953b3a65f07c2c4bc509afd73798930999f3
CapeKiller <mrmarcv@gmail.com>
2024-07-10 09:39:21 -0600

    New Update

--- 01-python-and-data-science/notebooks/2 - Day Two/turtle_challenge_1.py
+++ 01-python-and-data-science/notebooks/2 - Day Two/turtle_challenge_1.py
@@ -2,13 +2,57 @@ import turtle
 
 window = turtle.Screen()
 window.setup(width=800, height=800)
+window.bgcolor("black")
+window.title("Turtle Power")
 
 t = turtle.Turtle()
+t.speed(20)
 
 # Challenge 1: Squares in a Grid
 
 # YOUR CODE HERE
+t.penup()
+t.pencolor("white")
+t.setposition(-200, 250)
+#t.backward(300)
+t.pendown()
+for j in range (0,3):
+    t.penup()
+    for i in range (0,4):
+        t.pendown()
+        t.forward(100)
+        t.left(90)
+        t.penup()
+    t.penup()
+    t.forward(150)
+
+t.setposition(-200, 50)
+#t.backward(300)
+t.pendown()
+for j in range (0,3):
+    t.penup()
+    for i in range (0,4):
+        t.pendown()
+        t.forward(100)
+        t.left(90)
+        t.penup()
+    t.penup()
+    t.forward(150)
+
+t.setposition(-200, -150)
+#t.backward(300)
+t.pendown()
+for j in range (0,3):
+    t.penup()
+    for i in range (0,4):
+        t.pendown()
+        t.forward(100)
+        t.left(90)
+        t.penup()
+    t.penup()
+    t.forward(150)
 
 
 # DON'T TOUCH THIS
 turtle.mainloop()
+
--- 01-python-and-data-science/notebooks/2 - Day Two/turtle_challenge_2.py
+++ 01-python-and-data-science/notebooks/2 - Day Two/turtle_challenge_2.py
@@ -2,13 +2,67 @@ import turtle
 
 window = turtle.Screen()
 window.setup(width=800, height=800)
+window.bgcolor("black")
+window.title("Turtle Power")
 
 t = turtle.Turtle()
+t.speed(25)
+
 
 # Challenge 2: Circles in a Grid with One Missing
 
 # YOUR CODE HERE
+t.penup()
+t.pencolor("white")
+t.setposition(-200, 125)
+t.pendown()
+t.circle(50)
+
+t.penup()
+t.setposition(-50, 125)
+t.pendown()
+t.circle(50)
+
+t.penup()
+t.setposition(100, 125)
+t.pendown()
+t.circle(50)
+
+t.penup()
+t.setposition(-200, 0)
+t.pendown()
+t.circle(50)
+
+t.penup()
+t.setposition(-50, 0)
+t.pendown()
+t.circle(50)
+
+t.penup()
+t.setposition(100, 0)
+t.pendown()
+t.circle(50)
+
+t.penup()
+t.setposition(-50, -125)
+t.pendown()
+t.circle(50)
+
+t.penup()
+t.setposition(100, -125)
+t.pendown()
+t.circle(50)
+
+t.penup()
+t.setposition(-275, 250)
+t.pendown()
+
+for j in range (0,4):
+    t.forward(450)
+    t.right(90)
+
 
 
 # DON'T TOUCH THIS
 turtle.mainloop()
+
